198874607fd0fc841337a20929539dbf35375ff7
corentindaval <60287459+corentindaval@users.noreply.github.com>
2020-09-27 12:34:33 +0200

    rechargement des scripts

--- scripts/produit.js
+++ scripts/produit.js
@@ -29,14 +29,37 @@ function recupproduit(){
 function affichageproduit(){
 	recupproduit().then(function(json){
 	let tab = document.getElementById("affichage");
-		let buildoption="<option value=''>choisisez une couleur</option>";
+		let buildoption="";
 		for(let couleur of json.colors){
 			let nvoption="<option value='"+couleur+"'>"+couleur+"</option>";
 			buildoption=buildoption+nvoption;
 		}
-			let contenu="<img src='"+json.imageUrl+"'/><table><tr><td>nom:</td><td>"+json.name+"</td></tr><tr><td>prix:</td><td>"+json.price+"</td></tr><tr><td>description:</td><td>"+json.description+"</td></tr><tr><td>couleurs:</td><td><select id='selcouleur'>"+buildoption+"</select></td></tr></table>";
+			let contenu="<img src='"+json.imageUrl+"'/><table><tr><td>nom:</td><td>"+json.name+"</td></tr><tr><td>prix:</td><td>"+json.price+"</td></tr><tr><td>description:</td><td>"+json.description+"</td></tr><tr><td>couleurs:</td><td><select id='selcouleur'>"+buildoption+"</select></td></tr></table><button id='ajtpanier' data-id='"+json._id+"' data-name='"+json.name+"'>ajouter au panier</button><button id='rstpanier'>reset panier</button>";
 		tab.innerHTML=contenu;
+		const bouton=document.getElementById("ajtpanier");
+		bouton.addEventListener('click',ajouterAuPanier);
+		const rbouton=document.getElementById("rstpanier");
+		rbouton.addEventListener('click',resetPanier);
 	});
+}
+function ajouterAuPanier(e){
+	console.log(e.target.dataset.id);
+	let panier=getPanier();
+	let pexist=false;
+	for(let prod of panier){
+		if(prod.id ==e.target.dataset.id){
+			prod.nombre=prod.nombre+1;
+			pexist=true;
+		}
+	}
+	if(pexist==false){
+		panier.push({id:e.target.dataset.id,nom:e.target.dataset.name,nombre:1});
+	}
+	savePanier(panier);
+}
+
+function resetPanier(e){
+	viderPanier();
 }
 
 if(document.readyState==="complete"){
